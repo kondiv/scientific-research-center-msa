12226cbcd5f57ee5bb5b6e536cc819f31c090ff6
kondiv <ilyakondrachin77@gmail.com>
2025-10-27 15:31:04 +0300

    compose yaml

--- src/services/AuthenticationService/AuthenticationService.App/Common/Security/Tokens/JwtOptions.cs
+++ src/services/AuthenticationService/AuthenticationService.App/Common/Security/Tokens/JwtOptions.cs
@@ -3,8 +3,12 @@
 public class JwtOptions
 {
     public string Issuer { get; init; } = null!;
+    
     public string Audience { get; init; } = null!;
+    
     public string Key { get; init; } = null!;
+    
     public int RefreshTokenLifetime { get; init; }
+    
     public int AccessTokenLifetime { get; init; }
 }
--- src/services/AuthenticationService/AuthenticationService.App/Program.cs
+++ src/services/AuthenticationService/AuthenticationService.App/Program.cs
@@ -29,8 +29,6 @@ if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
