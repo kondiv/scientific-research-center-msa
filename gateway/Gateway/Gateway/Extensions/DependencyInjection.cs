85a9ca0eeda325fcdf33ba775fb38242a7fb9f9d
kondiv <ilyakondrachin77@gmail.com>
2025-10-27 17:38:00 +0300

    yarp gateway

---
+++ gateway/Gateway/Gateway/Extensions/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Gateway.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddGateway(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddReverseProxy().LoadFromConfig(configuration.GetSection("ReverseProxy"));
+        
+        return services;
+    }
+
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"]!,
+                    ValidAudience = configuration["Jwt:Audience"]!,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+                };
+
+                options.Events = new JwtBearerEvents()
+                {
+                    OnMessageReceived = context =>
+                    {
+                        if (context.Request.Cookies.TryGetValue("access_token", out var token))
+                        {
+                            context.Token = token;
+                        }
+                
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        
+        return services;
+    }
+
+    public static IServiceCollection AddPolicies(this IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .AddPolicy("Admin", policy => policy.RequireRole("ADMIN"));
+
+        return services;
+    }
+}
---
+++ gateway/Gateway/Gateway/Program.cs
@@ -0,0 +1,18 @@
+using Gateway.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddGateway(builder.Configuration);
+builder.Services.AddPolicies();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.Run();
