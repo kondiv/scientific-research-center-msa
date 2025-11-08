677a601c35463cfaba3808516f4bae1b62c8f1f6
kondiv <ilyakondrachin77@gmail.com>
2025-11-08 22:10:42 +0300

    change event: title property

--- src/shared/Shared/Shared.Contracts/ReportEvents/ReportCreatedEvent.cs
+++ src/shared/Shared/Shared.Contracts/ReportEvents/ReportCreatedEvent.cs
@@ -4,11 +4,14 @@ public sealed class ReportCreatedEvent
 {
     public Guid Id { get; private init; }
 
+    public string Title { get; private init; }
+
     public DateTime CreatedAtUtc { get; private init; }
 
-    public ReportCreatedEvent(Guid id, DateTime createdAtUtc)
+    public ReportCreatedEvent(Guid id, string title, DateTime createdAtUtc)
     {
         Id = id;
+        Title = title;
         CreatedAtUtc = createdAtUtc;
     }
 }
